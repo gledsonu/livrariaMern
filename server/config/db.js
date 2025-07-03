2f7deef6b1f6deabbf058508cb75453b2a87d307
gledsonu <gledsonurzedo@gmail.com>
2025-07-03 20:10:54 -0300

    arq dotenv

--- server/config/db.js
+++ server/config/db.js
@@ -1,36 +1,14 @@
-/* const mongoose = require('mongoose');
-
-const config = require('config'); 
-
-const db = config.get('mongoURI');
-
-const connectDB = async () => {
-    try {
-        mongoose.set('strictQuery', true);
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-        });
-
-        console.log('MongoDB esta conectado...')
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB; */
-
 const mongoose = require('mongoose');
 
-const config = require('config');
+require('dotenv').config()
 
-const db = config.get('mongoURI');
+const uri = process.env.MONGODB_URI
 
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', true);
 
-        await mongoose.connect(db, {
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
         });
 
